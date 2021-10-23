0596b91d48a51cd3430dc56f23465b701b0c5a5f
Tomliak1992 <86728264+Tomliak1992@users.noreply.github.com>
2021-10-23 11:27:44 +0300

    23/10/2021

--- js/mod3.js
+++ js/mod3.js
@@ -180,5 +180,131 @@ if (apartment.hasOwnProperty(key)){
   
 }
 
-// 12/41
+// 13/41
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const values = [];
+// Change code below this line
+const keys = Object.keys(apartment);
+for (const key of keys){
+  values.push(apartment[key])
+} 
+
+// 15/41
+
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
+
+// 16/41
+
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+const values = Object.values(salaries);
+
+for (const value of values){
+  totalSalary += value
+}
+  // Change code above this line
+  return totalSalary;
+}
+
+// 17/41
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+for (const color of colors) {
+  hexColors.push(color.hex)
+
+  console.log(color)
+}
+for (const color of colors) {
+  rgbColors.push(color.rgb)
+
+  console.log(color)
+}
+
+// 18/41
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+for (const product of products){
+  if (productName === product.name) {
+    return product.price}
+}
+return null
+  // Change code above this line
+}
+
+// 19/41
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+const result = [];
+
+for(const el of products) {
+  if (el.hasOwnProperty(propName))
+  result.push(el[propName])
+  }
+  return result
+}
+
+// 20/41
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Пиши код ниже этой строки
+for (product of products) {
+  if (product.name === productName) {
+    return product.price * product.quantity
+  }
+}
+
+return 0
+  // Пиши код выше этой строки
+}
+
+// 21/41
+
+
+
 

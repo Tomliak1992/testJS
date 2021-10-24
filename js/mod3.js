cb84afc99854bfe251ac12db8115fa9039874226
Tomliak1992 <tomlakevgenii@gmail.com>
2021-10-24 23:34:45 +0300

    24/10/21

--- js/mod3.js
+++ js/mod3.js
@@ -305,6 +305,247 @@ return 0
 
 // 21/41
 
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+const { yesterday, today, tomorrow } = highTemperatures
 
 
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// 22/41
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+const { yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures
+
+// Change code above this line
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// 23/41
+
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Change code below this line
+
+
+const { yesterday: highYesterday, today: highToday, tomorrow: highTomorrow, icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } = highTemperatures
+// Change code above this line
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+
+// 24/41
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (const { hex, rgb } of colors) {
+   
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+  
+  console.log(hex);
+  console.log(rgb);
+}
+
+// 25/41
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Change code below this line
+
+const { 
+  today: { 
+    low: lowToday, 
+    high: highToday, 
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" 
+  }, 
+  tomorrow: { low: lowTomorrow, high: highTomorrow, icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" } 
+  } = forecast;
+
+  // 26/41
+
+  // Change code below this line
+function calculateMeanTemperature(forecast) {
+
+  const { 
+    today: { 
+      low: todayLow, 
+      high: todayHigh, 
+    }, 
+    tomorrow: { 
+      low: tomorrowLow, 
+      high: tomorrowHigh,
+   } 
+   } = forecast
+    // Change code above this line
+    return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+  }
+
+  // 27/41
+
+  const scores = [89, 64, 42, 17, 93, 51, 26];
+
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+// 28/41
+
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+// 29/41
+
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+// Change code below this line
+const finalSettings = {
+...defaultSettings,
+...overrideSettings
+
+};
+
+// 30/41
+
+function makeTask(data) {
+  const completed = false;
+  const category = "General";
+  const priority = "Normal";
+  // Change code below this line
+return {completed, priority, category, ...data}
+  // Change code above this line
+}
+
+// 32/41
+
+// Change code below this line
+function addOverNum(maxNumber, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    if (arg > maxNumber) {
+      total += arg;
+    }
+  }
+
+  return total;
+  // Change code above this line
+}
+
+
+// 33/41
+
+function findMatches(array, ...args) {
+  const matches = []; // Don't change this line
+for (const number of args) {
+if (array.includes(number))
+{matches.push(number)}}
+  // Change code above this line
+  return matches;
+}
+
+// 34/41
+
+const bookShelf = {
+  // Change code below this line
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  }
+  // Change code above this line
+};
+
+// 35/41
+
+const bookShelf = {
+  books: ["The last kingdom", "Haze", "The guardian of dreams"],
+  updateBook(oldName, newName) {
+    // Change code below this line
+let bookIndex = this.books.indexOf(oldName);
+let newBook = this.books.splice(bookIndex, 1, newName)
+
+
+    // Change code above this line
+  },
+};
+
+
+// 36/41
+
+const atTheOldToad = {
+  // Change code below this line
+potions: [],
+
+
+  // Change code above this line
+};
+
+
+// 37/41
+
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+ getPotions() {
+ console.log(this);
+  
+ return this.potions
+ }
+
+};
 
